e888faa086754d0bf710834456fa885075fc6da2
Madhushraba <madhushraba21das@gmail.com>
2023-07-19 10:08:41 +0530

    'yup'

--- src/components/ui/Hotpizza.jsx
+++ src/components/ui/Hotpizza.jsx
@@ -26,33 +26,54 @@ const Hotpizza = () => {
       </section> */}
       <h2>Hot Pizza</h2>
       <div className="hotpizz">
-        
         <div className="ser">
-          <img className="imghot" src="https://images.pexels.com/photos/16962445/pexels-photo-16962445/free-photo-of-beef-burgers-in-hands.jpeg?auto=compress&cs=tinysrgb&w=600" alt=" imgg" />
+          <img
+            className="imghot"
+            src="https://images.pexels.com/photos/16962445/pexels-photo-16962445/free-photo-of-beef-burgers-in-hands.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=" imgg"
+          />
           <h4>Double Cheese Margherita</h4>
           <p>Lorem ipsum dolor sit.</p>
-          <Button>+cart</Button>
+          <div className="hotbox">
+            <span>$112</span> <Button>+cart</Button>
+          </div>{" "}
         </div>
         <div className="ser">
-          <img className="imghot" src="https://images.pexels.com/photos/1199957/pexels-photo-1199957.jpeg?auto=compress&cs=tinysrgb&w=600" alt=" imgg" />
+          <img
+            className="imghot"
+            src="https://images.pexels.com/photos/1199957/pexels-photo-1199957.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=" imgg"
+          />
           <h4>Royal Cheese Burger</h4>
           <p>Lorem ipsum dolor sit.</p>
-          <Button>+cart</Button>
+          <div className="hotbox">
+            <span>$100</span> <Button>+cart</Button>
+          </div>
         </div>
         <div className="ser">
-          <img className="imghot" src="https://images.pexels.com/photos/3764353/pexels-photo-3764353.jpeg?auto=compress&cs=tinysrgb&w=600" alt=" imgg" />
+          <img
+            className="imghot"
+            src="https://images.pexels.com/photos/3764353/pexels-photo-3764353.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=" imgg"
+          />
           <h4>Pizza With Mushroom</h4>
           <p>Lorem ipsum dolor sit.</p>
-          <Button>+cart</Button>
+          <div className="hotbox">
+            <span>$125</span> <Button>+cart</Button>
+          </div>
         </div>
         <div className="ser">
-          <img className="imghot" src="https://images.pexels.com/photos/2067403/pexels-photo-2067403.jpeg?auto=compress&cs=tinysrgb&w=600" alt=" imgg" />
+          <img
+            className="imghot"
+            src="https://images.pexels.com/photos/2067403/pexels-photo-2067403.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=" imgg"
+          />
           <h4>Pizza With Mushroom</h4>
           <p>Lorem ipsum dolor sit.</p>
-          <Button>+cart</Button>
+          <div className="hotbox">
+            <span>$160</span> <Button>+cart</Button>
+          </div>
         </div>
-        
-       
       </div>
     </div>
   );
